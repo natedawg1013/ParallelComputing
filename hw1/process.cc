6a1c134da929b94683461a7b483dcf0e5754df1a
Nathan Bernard <natedawg1013@gmail.com>
2017-01-30 17:18:30 -0500

    Working parallel processing of first 3 million lines

--- hw1/process.cc
+++ hw1/process.cc
@@ -4,21 +4,46 @@
 #include <cstring>
 
 int main(int argc, char* argv[]){
+  int nThreads = omp_get_max_threads();
+  int linPerThd = 100000;
   FILE* data = fopen(argv[1],"r");
+  perror("");
   char* line = NULL;
-  size_t count=299;
+  char** lines = new char*[linPerThd*nThreads];
+  for(int i=0;i<linPerThd*nThreads;++i) lines[i]=NULL;
+  size_t count=300;
   getline(&line, &count, data);
-  count=300;
-  getline(&line, &count, data);
-  char *ptr, *current;
-  char *start, *end, *length;
-  current=strtok_r(line, ",", &ptr);
-  for(int i=0;i<5;i++){
-    if(i==1) start=strdup(current);
-    if(i==2) end=strdup(current);
-    if(i==4) length=strdup(current);
-    current=strtok_r(NULL, ",", &ptr);
+  printf("Reading %d lines\n", linPerThd*nThreads);
+  for(int i=0;i<linPerThd*nThreads;++i){
+    count=300;
+    getline(&(lines[i]), &count, data);
+    if(i%(linPerThd*nThreads/10)==0){
+      printf("|");
+      fflush(stdout);
+    }
   }
-  printf("%s/%s/%s\n", start, end, length);
+  printf("Done\n");
+  double total = 0.0;
+  #pragma omp parallel for
+  for(int j=0;j<nThreads;j++){
+    double subtotal=0.0;
+    for(int k=0;k<linPerThd;k++){
+      char *ptr, *current;
+      char *start, *end;
+      double length;
+      current=strtok_r(lines[j*linPerThd+k], ",", &ptr);
+      for(int i=0;i<5;i++){
+        //if(i==1) start=strdup(current);
+        //if(i==2) end=strdup(current);
+        if(i==4) length=atof(current);
+        current=strtok_r(NULL, ",", &ptr);
+      }
+      subtotal+=length;
+    }
+    #pragma omp atomic
+    total+=subtotal;
+  }
+  printf("Average trip length: %f\n", total/(linPerThd*nThreads));
+    
   return 0;
 }
