9bce8cc0d1f3b31fd6fe544696c4df748d8c2393
Nathan Bernard <natedawg1013@gmail.com>
2017-02-08 22:23:04 -0500

    Matrix multiplication for HW2

---
+++ hw2/mtx_mp.cc
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <ctime>
+#include <iostream> 
+#include <omp.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+  volatile int a[100][100];
+  volatile int b[100][100];
+  volatile int c[100][100];
+
+  for(int i=0;i<10;i++){
+    for(int j=0;j<100;j++){
+      a[i][j]=b[i][j]=1;
+    }
+  }
+  
+  clock_t begin = clock();
+#pragma omp parallel for
+  for(int i=0;i<100;i++){
+    for(int j=0;j<100;j++){
+      c[i][j]=a[i][j]*b[i][j];
+    }
+  }
+  clock_t end = clock();
+
+  cout<<"Time elapsed: "<<(double(end-begin)/CLOCKS_PER_SEC)<<" seconds"<<endl;
+
+  return 0;
+}
